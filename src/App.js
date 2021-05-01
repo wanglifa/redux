09c6ae137a1d887f8a3ea649f8254fecce78971d
wanglifa <wanglifa1995@gmail.com>
2021-05-01 14:47:35 +0800

    feat: 使用 createContext 创建全局读写 api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useState, useContext} from 'react';
+
+const appContext = createContext(null)
 
 function App() {
+  const [appState, setAppState] = useState({
+    user: {
+      name: 'lifa',
+      age: 18
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <appContext.Provider value={{appState, setAppState}}>
+      <大老婆/>
+      <二老婆/>
+      <三老婆/>
+    </appContext.Provider>
+  );
+}
+
+const 大老婆 = () => {
+  return <div>大老婆</div>;
+}
+const 二老婆 = () => {
+  return (
+    <div>
+      <div>二老婆</div>
+      <User/>
     </div>
   );
+}
+const 三老婆 = () => {
+  return (
+    <div>
+      <div>三老婆</div>
+      <UserModify/>
+    </div>
+  );
+}
+const User = () => {
+  const {appState} = useContext(appContext);
+  return (
+    <div>
+      {appState.user.name}
+    </div>
+  )
+}
+const UserModify = () => {
+  const {appState, setAppState} = useContext(appContext);
+  const onChange = (event) => {
+    setAppState({
+      user: {
+        ...appState.user,
+        name: event.target.value,
+      },
+    });
+  }
+  return (
+    <div>
+      <input value={appState.user.name} onChange={onChange}/>
+    </div>
+  )
 }
 
 export default App;
